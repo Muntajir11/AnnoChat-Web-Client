9d5c5fafeba272739a78d2e2bbbf89bd1ca7b020
Muntajir <muntajirwork11@gmail.com>
2025-05-20 19:34:28 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://annochat.social',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  generateIndexSitemap: true,
+};
